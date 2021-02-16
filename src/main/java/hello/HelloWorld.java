cbc4de906e4edbd1ae1447713c7a32ec4ccc94f1
rav95054 <mallampet2021@gmail.com>
2021-02-15 18:55:11 -0800

    file chnages

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,8 +1,5 @@
-package hello;
-
-public class HelloWorld {
-  public static void main(String[] args) {
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
-  }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
